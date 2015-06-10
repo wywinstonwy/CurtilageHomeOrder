f1ea164c60af2bfb6f56a059c949f9fded0b110a
王云 <wangyun@qctt.cn>
2015-06-10 09:54:56 +0800

    是滴是滴

---
+++ CurtilageHomeOrder/viewControllers/OrderMealVC/CKFoodCell.h
@@ -0,0 +1,13 @@
+//
+//  CKFoodCell.h
+//  CurtilageHomeOrder
+//
+//  Created by wy on 15/6/9.
+//  Copyright (c) 2015年 WangYun. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface CKFoodCell : UITableViewCell
+
+@end
---
+++ CurtilageHomeOrder/viewControllers/OrderMealVC/CommentCell.h
@@ -0,0 +1,13 @@
+//
+//  CommentCell.h
+//  CurtilageHomeOrder
+//
+//  Created by wy on 15/6/9.
+//  Copyright (c) 2015年 WangYun. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface CommentCell : UITableViewCell
+
+@end
